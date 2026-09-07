5bed82e4210fa2d021f7c4f34d9c87524c415863
Jaimos Skriletz <jaimosskriletz@gmail.com>
2026-09-06 23:01:20 -0600

    Add links to test problems on Grades page.
    Add links to the problems for tests on the Grades page if a student
    can access the test version. This also removes any links to a proctored
    test version on the grades page. This page is links for students, and
    they cannot access a proctored test without a proctor password.
    
    This also adds the `window.scrollY/X` to the position of the MathJax
    loaderOverlay to correctly position it when linking to a anchor.

--- htdocs/js/MathJaxConfig/mathjax-config.js
+++ htdocs/js/MathJaxConfig/mathjax-config.js
@@ -188,8 +188,8 @@ if (!window.MathJax) {
 		loaderOverlay.classList.add('problem-content');
 		const bodyRectangle = problemContent.getBoundingClientRect();
 		loaderOverlay.style.position = 'absolute';
-		loaderOverlay.style.top = `${bodyRectangle.y}px`;
-		loaderOverlay.style.left = `${bodyRectangle.x}px`;
+		loaderOverlay.style.top = `${bodyRectangle.top + window.scrollY}px`;
+		loaderOverlay.style.left = `${bodyRectangle.left + window.scrollX}px`;
 		loaderOverlay.style.width = `${bodyRectangle.width}px`;
 		loaderOverlay.style.height = `${bodyRectangle.height}px`;
 		loaderOverlay.style.overflow = 'clip';
@@ -204,8 +204,8 @@ if (!window.MathJax) {
 		problemContent.after(loaderOverlay);
 		const resizeObserver = new ResizeObserver(() => {
 			const bodyRectangle = problemContent.getBoundingClientRect();
-			loaderOverlay.style.top = `${bodyRectangle.top}px`;
-			loaderOverlay.style.left = `${bodyRectangle.left}px`;
+			loaderOverlay.style.top = `${bodyRectangle.top + window.scrollY}px`;
+			loaderOverlay.style.left = `${bodyRectangle.left + window.scrollX}px`;
 			loaderOverlay.style.width = `${bodyRectangle.width}px`;
 			loaderOverlay.style.height = `${bodyRectangle.height}px`;
 		});
